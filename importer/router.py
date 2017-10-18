35ab6be8c48e3166a4fd3871851501510a6a8a25
Max Melzer <max.melzer@netresearch.de>
2017-10-18 17:23:56 +0200

    Json router improvements

--- importer/router.py
+++ importer/router.py
@@ -3,7 +3,10 @@ import importer
 
 # Check if a given json string is in legacy format
 def getDays(json):
-    if type(json[1]) is dict:
-        return importer.DaysFactory(json)
-    else:
+    if 'start' in json[0]:
         return importer.LegacyDaysFactory(json)
+    elif 'work' in json[0]:
+        return importer.DaysFactory(json)        
+    else:
+        print 'Critical Error: Could not determine type of json file'
+        exit(2)
